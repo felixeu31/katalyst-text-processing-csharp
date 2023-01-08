329f86f15a2691633b742c66c8fc2cfd5ed071ac
Félix Díez <felixeu31@gmail.com>
2023-01-08 19:04:36 +0100

    chore: create new project for implementation

--- TextProcessing/TextProcessing.Tests/TextProcessorTests.cs
+++ TextProcessing/TextProcessing.Tests/TextProcessorTests.cs
@@ -26,12 +26,5 @@ namespace TextProcessing.Tests
         }
     }
 
-    public class TextProcessor
-    {
-        public int CountNumberOfWords(string textSample1)
-        {
-            int numberOfWords = textSample1.Split(" ").Length;
-            return numberOfWords;
-        }
-    }
+    
 }
---
+++ TextProcessing/TextProcessing/TextProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace TextProcessing
+{
+    public class TextProcessor
+    {
+        public int CountNumberOfWords(string textSample1)
+        {
+            int numberOfWords = textSample1.Split(" ").Length;
+            return numberOfWords;
+        }
+    }
+}
